d6ad0ef52ba30633896a2152b98d68e4c0c4e89b
H. M. Fahad <45889184+hmfahad@users.noreply.github.com>
2023-03-10 12:30:19 +0600

    Data base Creation and Insertion table updated

--- db_ch03-2.php
+++ db_ch03-2.php
@@ -1,11 +1,10 @@
 <?php
 //connet to mySQL
 
-<<<<<<< HEAD
 include("connection.php");
 
-   /*
 
+echo "<br>";
     //insert data into the movie table
 
     $sql = "INSERT INTO movie
@@ -18,44 +17,17 @@ include("connection.php");
 
     if ($conn->query($sql) === TRUE ){
         echo "Movie Table record created successfully";
+        echo "<br>";
     } else {
         echo "Error in movie table: " . $sql . "<br>" . $conn->error;
+        echo "<br>";
     }
 
-    
 
-    */
-=======
-$conn = new mysqli ('localhost', 'root','', 'moviesite');
-    
-        if ($conn->connect_error){
-                die("Connection failed: " . $conn->connect_error);
-        }
-
-    
-
-    //insert data into the movie table
-
-    $sql = 'INSERT INTO movie
-            (movie_id, movie_name, movie_type, movie_year, movie_leadactor,
-            movie_director)
-            VALUES
-            (1, "...bruce", 5, 2003, 1, 2),
-            (2, "Office Space", 5, 1999, 5, 6),
-            (3, "Grand Canyon", 2, 1991, 4, 3)';
-
-    if ($conn->query($sql) === TRUE){
-        echo "Movie table record Created Successfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-
-    }
->>>>>>> f6ef0905f994139a90ebe3976f666af0a67aa5eb
-
+   
     //insert data into the movietype table
 
-/*
-<<<<<<< HEAD
+
     $sq = "INSERT INTO movietype
                 (movietype_id, movietype_lable)
         VALUES  
@@ -70,33 +42,15 @@ $conn = new mysqli ('localhost', 'root','', 'moviesite');
 
         if ($conn->query($sq) === TRUE ){
                 echo "Movietype Table Createed Successfull";
+                echo "<br>";
         } else {
                 echo "Error of movie type table:" . $sq . "<br>" . $conn->error;
+                echo "<br>";
         }
 
-  */      
-=======
-    $sql = 'INSERT INTO movietype
-                (movietype_id, movietype_label)
-        VALUES  
-                (1, "Sci Fi"),
-                (2, "Drama"),
-                (3, "Adventure"),
-                (4, "War"),
-                (5, "Comedy"),
-                (6, "Horror"),
-                (7, "Action"),
-                (8, "Kids")';
 
-        if ($conn->query($sql) === TRUE){
-                echo "Movietype table record Created Successfully";
-        } else {
-                echo "Error: " . $sql . "<br>" . $conn->error;
->>>>>>> f6ef0905f994139a90ebe3976f666af0a67aa5eb
+        //Insert data into people table
 
-        }
-
-<<<<<<< HEAD
                 $s = "INSERT INTO people
                         (people_id, people_fullname, people_isactor, people_isdirector)
                 VALUES
@@ -109,11 +63,11 @@ $conn = new mysqli ('localhost', 'root','', 'moviesite');
 
                 if ($conn->query($s) === TRUE){
                         echo "Connection success of people table";
+                        echo "<br>";
                 } else {
-                        echo "Error in people table" . $conn . "<br>" . $s->error;
+                        echo "Error in people table" . $s . "<br>" . $conn->error;
+                        echo "<br>";
                 }
-=======
-        */
           
         //insert data into the people table
 
@@ -129,16 +83,12 @@ $conn = new mysqli ('localhost', 'root','', 'moviesite');
 
         if ($conn->query($sql) === TRUE){
                  echo "People table record Created Successfully";
+                 echo "<br>";
         } else {
                  echo "Error: " . $sql . "<br>" . $conn->error;
+                 echo "<br>";
 
         }
->>>>>>> f6ef0905f994139a90ebe3976f666af0a67aa5eb
-
-                echo "Data inserted Successfully!";
-
-
-
 
 $conn->close();
 
